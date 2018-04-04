61c5f03f31023d51141cac17e12d5bd80c6e14e7
yuantang126 <yuantang126@126.com>
2018-04-04 17:57:47 +0800

    logo

--- app/src/main/java/com/coder/binauralbeats/activity/WelcomeActivity.java
+++ app/src/main/java/com/coder/binauralbeats/activity/WelcomeActivity.java
@@ -1,41 +1,60 @@
 package com.coder.binauralbeats.activity;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
 
 import com.coder.binauralbeats.R;
-import com.coder.binauralbeats.base.BaseActivity;
-import com.coder.binauralbeats.basemvp.MvpBasePresenter;
-import com.coder.binauralbeats.basemvp.MvpBaseView;
+import com.coder.binauralbeats.activity.home.HomeActivity;
 
-public class WelcomeActivity extends BaseActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WelcomeActivity extends AppCompatActivity {
+    @BindView(R.id.welcome_img)
+    ImageView welcomeImg;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setBackgroundDrawable(null);
+        //去除标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //去除状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcom);
-    }
-    @Override
-    protected int getLayout() {
-        return 0;
+        ButterKnife.bind(this);
+        startWelcome();
     }
 
-    @Override
-    protected MvpBasePresenter createPresenter() {
-        return null;
+    public void startWelcome() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(welcomeImg, "translationY",-200);
+        animator.setDuration(1000);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                startActivity(new Intent(WelcomeActivity.this, HomeActivity.class));
+                finish();
+            }
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+        });
+        animator.start();
     }
 
-    @Override
-    protected MvpBaseView createView() {
-        return null;
-    }
-
-    @Override
-    protected void superInit(Intent intent) {
-
-    }
-
-    @Override
-    protected void initEventAndData() {
-
-    }
 }
